4c17724fc5286b81d1003ff8f3502eb406141a1e
Salsyekys <salllwaduh@gmail.com>
2026-03-17 04:05:12 +0700

    Fixed mobile UI (cart button not displaying)

--- frontend-pos/src/views/customer/index.jsx
+++ frontend-pos/src/views/customer/index.jsx
@@ -259,7 +259,7 @@ export default function CustomerHome() {
 
                     <div className="navbar-nav flex-row order-md-last gap-3 align-items-center">
                         {/* Cart Button */}
-                        <div className="nav-item d-none d-sm-block">
+                        <div className="nav-item">
                             <button className="btn btn-ghost-primary rounded-pill position-relative" data-bs-toggle="offcanvas" data-bs-target="#cartOffcanvas" aria-controls="cartOffcanvas">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
                                 {cartItems.length > 0 && (
